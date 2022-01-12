36c2a8be7bc97a41ffde9faaea041789cf73f075
Justin Jeffries <justincyjeffries@gmail.com>
2022-01-12 00:32:38 -0700

    commit tree 4d0cf800be73aa55493c50aa1fbdc301dbe2512b

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1 = 31;
+const num2 = 2;
+const num3 = 24;
+const num4 = 5
+let multiply = (num1 * num2);
 
-// Write your code here
+let random = Math.floor(Math.random() * 100) + 1;
+
+let mod = (num3 % num4);
+
+let max = Math.max(1, 2, 3, 4, 5, 20);
